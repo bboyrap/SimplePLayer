35c2aed5fa385ea8cf5788be8855aab9a7244708
Mateus Nascimento <mateusnascimento5.1@gmail.com>
2016-05-04 00:37:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ultrabook/simpleplayer/SearchActivity.java
@@ -0,0 +1,112 @@
+package com.example.ultrabook.simpleplayer;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private List<Video> searchResults;
+
+    private EditText searchInput;
+    private ListView videosFound;
+
+    private Handler handler;
+
+    private Button butao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        searchInput = (EditText)findViewById(R.id.search_input);
+        videosFound = (ListView)findViewById(R.id.videos_found);
+        butao       = (Button)findViewById(R.id.button);
+
+        butao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchOnYoutube(searchInput.getText().toString());
+            }
+        });
+
+        handler = new Handler();
+
+
+
+    }
+
+
+    private void searchOnYoutube(final String keywords){
+        new Thread(){
+            public void run(){
+                YoutubeConnector yc = new YoutubeConnector(SearchActivity.this);
+                searchResults = yc.search(keywords);
+                handler.post(new Runnable(){
+                    public void run(){
+                        updateVideosFound();
+                    }
+                });
+            }
+        }.start();
+    }
+
+
+    private void addClickListener(){
+        videosFound.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
+                intent.putExtra("VIDEO_ID", searchResults.get(pos).getId());
+                startActivity(intent);
+            }
+
+        });
+    }
+
+
+    private void updateVideosFound(){
+        ArrayAdapter<Video> adapter = new ArrayAdapter<Video>(getApplicationContext(), R.layout.video_item, searchResults){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.video_item, parent, false);
+                }
+                ImageView thumbnail = (ImageView)convertView.findViewById(R.id.video_thumbnail);
+                TextView title = (TextView)convertView.findViewById(R.id.video_title);
+                TextView description = (TextView)convertView.findViewById(R.id.video_description);
+
+                Video searchResult = searchResults.get(position);
+
+                Picasso.with(getApplicationContext()).load(searchResult.getThumbnailURL()).into(thumbnail);
+                title.setText(searchResult.getTitle());
+                description.setText(searchResult.getDescription());
+                return convertView;
+            }
+        };
+
+        videosFound.setAdapter(adapter);
+    }
+
+
+}
